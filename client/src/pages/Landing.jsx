221de34fb2659052b9b2f31c321479a3844fe6b7
ShaneC0 <connors.shane@hotmail.com>
2020-11-16 11:39:22 -0500

    yeah we good

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,10 +1,7 @@
 export default function Landing() {
   return (
     <div id="landing">
-      <div className="image">
-        <h3>Learning</h3>
-        <h1>Reimagined.</h1>
-      </div>
+      hey
     </div>
   );
 }
